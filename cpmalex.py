d05602386843fd55e61a089257c5eec37c5c826f
CPMAlex1999 <cpmalex1999@gmail.com>
2025-02-21 22:28:56 +0500

    Update and rename cpmayan.py to cpmalex.py

--- cpmalex.py
+++ cpmalex.py
@@ -1,8 +1,8 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://cpmayan.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://cpmalex.squareweb.app/api"
 
-class CPMAyan:
+class CPMAlex:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
